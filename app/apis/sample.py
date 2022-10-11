15f7ee1c44b18c221ba79331b46d805d9fbf40d7
Bin Sheng Ang <bsa220@ic.ac.uk>
2022-10-11 17:28:59 +0100

    Added SQLAlchemy with sqllite support for dev

--- app/apis/sample.py
+++ app/apis/sample.py
@@ -1,9 +1,34 @@
 from app import api
+from app.database import db 
+from flask import request, abort, jsonify
 from flask_restx import Resource
+
+
+
+# Database
+from app.models.example import SampleData
+from app.schemas.example import SampleDataSchema
 
 sample_api = api.namespace("sample", description="Test API")
 
-@sample_api.route('/', methods=['GET'])
+sample_data_serializer = SampleDataSchema()
+
+
+@sample_api.route('/', methods=['GET', 'POST'])
 class TestApi(Resource):
     def get(self):
-        return { 'message': 'hello!' }
+        data = SampleData.query.all()
+        result = [sample_data_serializer.dump(d) for d in data]
+        return result
+
+    def post(self):
+        if not request.is_json:
+            abort(406, description="NOT JSON")
+
+        # print(request.json["count"])
+        data = { 'message': 'in progress' }
+
+        new_sample_data = SampleData(count=request.json["count"])
+        db.session.add(new_sample_data)
+        db.session.commit()
+        return data, 200
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from app import create_app
+from app.database import db
 
 # APIs
 from app.apis.sample import sample_api
