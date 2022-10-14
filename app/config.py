7f1d1b037dc8d6d8f91f1129353a24cade51cf84
Bin Sheng Ang <bsa220@ic.ac.uk>
2022-10-14 09:45:47 +0100

    Fix fetching STAGING_DB_PASSWORD

--- app/config.py
+++ app/config.py
@@ -12,10 +12,11 @@ class DevConfig(BaseConfig):
 class StagingConfig(BaseConfig):
     STAGING_DB = os.environ.get("STAGING_DB", None)
     STAGING_DB_USER = os.environ.get("STAGING_DB_USER", None)
-    STAGING_DB_PASSWORD = os.environ.get("STAGING_DB_USER", None)
+    STAGING_DB_PASSWORD = os.environ.get("STAGING_DB_PASSWORD", None)
 
     if STAGING_DB_PASSWORD is not None and STAGING_DB_USER is not None and STAGING_DB is not None:
         SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{STAGING_DB_USER}:{STAGING_DB_PASSWORD}@db.doc.ic.ac.uk:5432/{STAGING_DB}"
+        print(SQLALCHEMY_DATABASE_URI)
     else:
-        SQLALCHEMY_DATABASE_URI = "sqlite:///dev.db"
+        SQLALCHEMY_DATABASE_URI = "sqlite:///staging.db"
         
