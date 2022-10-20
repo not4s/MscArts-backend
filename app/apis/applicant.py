3ada10d02c88be1501266109cf4792a8dbcbc988
Matt Neave <matthewneaveis@hotmail.co.uk>
2022-10-20 12:46:05 +0100

    Added summation to application api

--- app/apis/applicant.py
+++ app/apis/applicant.py
@@ -40,12 +40,15 @@ class ApplicantApi(Resource):
             return data, 200
 
         count = request.args.get("count", default=None, type=str)
+        combined = 0
         if count:
             df = pd.DataFrame(data)
             counted = df[count].value_counts()
             reformatted = []
             for key, value in counted.items():
                 reformatted.append({count: key, "count": int(value)})
+                combined += int(value)
+            reformatted.append({count: "Combined", "count": combined})
 
             return reformatted, 200
         return data, 200
