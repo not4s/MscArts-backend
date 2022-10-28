28c4e4558433381b806aaa7627a413405b829841
Bin Sheng Ang <bsa220@ic.ac.uk>
2022-10-28 11:34:07 +0100

    Updated Applicant API to only retrieve latest version for each erpid

--- app/apis/applicant.py
+++ app/apis/applicant.py
@@ -3,6 +3,7 @@ from app.database import db
 from app.models.applicant import Applicant, Target
 from app.schemas.applicant import ApplicantSchema, TargetSchema
 from app.apis.user import read_access_required
+from sqlalchemy.sql import func, and_
 from flask import request, abort
 from flask_restx import Resource
 import pandas as pd
@@ -38,8 +39,8 @@ class ApplicantApi(Resource):
 
         # TODO: track the progress
         for i, target in enumerate(data):
-            data[i] = {**target, "progress" : random.randint(1, 100)}
-        
+            data[i] = {**target, "progress": random.randint(1, 100)}
+
         return data, 200
 
     def post(self):
@@ -109,11 +110,34 @@ class ApplicantAttributeApi(Resource):
 
         return data, 200
 
+
+# def get_latest_version():
+#     query = db.session.query(Applicant.erpid, func.max(Applicant.version).label('version')).group_by(Applicant.erpid)
+#     return query.all()
+
+
 @applicant_api.route("/", methods=["GET"])
 class ApplicantApi(Resource):
     @read_access_required
     def get(self):
+        latest_version = (
+            db.session.query(
+                Applicant.erpid.label("erpid"),
+                func.max(Applicant.version).label("version"),
+            )
+            .group_by(Applicant.erpid)
+            .subquery()
+        )
+
         query = Applicant.query
+
+        query = query.join(
+            latest_version,
+            and_(
+                latest_version.c.version == Applicant.version,
+                latest_version.c.erpid == Applicant.erpid,
+            ),
+        )
 
         for (col, col_type) in filters:
             filter_value = request.args.get(col, default=None, type=col_type)
@@ -124,38 +148,62 @@ class ApplicantApi(Resource):
                 elif col_type is int:
                     query = query.filter(Applicant.__dict__[col] == filter_value)
 
+        # query = query.filter().group_by(Applicant.erpid)
+
         data = [applicant_deserializer.dump(d) for d in query.all()]
         if len(data) == 0:
             return data, 200
 
         count = request.args.get("count", default=None, type=str)
-        series = request.args.get("series", default='application_status', type=str)
+        series = request.args.get("series", default="application_status", type=str)
 
         if count and series:
             df = pd.DataFrame(data)
             counted = df[[count, series]].value_counts()
             reformatted = []
             for key, value in counted.items():
-                if (key[0].strip() != ""):
-                    combined = list(filter(lambda x: x[count] == 'Combined' and x["series"] == key[1], reformatted))
+                if key[0].strip() != "":
+                    combined = list(
+                        filter(
+                            lambda x: x[count] == "Combined" and x["series"] == key[1],
+                            reformatted,
+                        )
+                    )
                     if len(combined) > 0:
                         reformatted.remove(combined[0])
                         reformatted.append(
-                            {count: "Combined", "count": int(value) + combined[0]['count'], "type": key[0], "series": key[1]}
+                            {
+                                count: "Combined",
+                                "count": int(value) + combined[0]["count"],
+                                "type": key[0],
+                                "series": key[1],
+                            }
                         )
                     else:
                         reformatted.append(
-                            {count: "Combined", "count": int(value), "type": key[0], "series": key[1]}
+                            {
+                                count: "Combined",
+                                "count": int(value),
+                                "type": key[0],
+                                "series": key[1],
+                            }
                         )
-                    reformatted.append({count: key[0], "count": int(value), "type": key[0], "series": key[1]})
+                    reformatted.append(
+                        {
+                            count: key[0],
+                            "count": int(value),
+                            "type": key[0],
+                            "series": key[1],
+                        }
+                    )
             return reformatted, 200
-        
+
         if count:
             df = pd.DataFrame(data)
             counted = df[count].value_counts()
             reformatted = []
             for key, value in counted.items():
-                if (key.strip() != ""):
+                if key.strip() != "":
                     reformatted.append(
                         {count: "Combined", "count": int(value), "type": key}
                     )
