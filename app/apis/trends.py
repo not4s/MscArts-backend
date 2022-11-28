d7dfc70b757333cb2bc1a606c238c757ed6f0f76
Dana Lu <dl1820@imperial.ac.uk>
2022-11-28 12:13:07 +0000

    Added new API for getting trends per cycle

--- app/apis/trends.py
+++ app/apis/trends.py
@@ -7,7 +7,6 @@ from flask import request
 from flask_restx import Resource
 from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
-import pandas as pd
 
 trends_api = api.namespace("api/trends", description="Trends API")
 
@@ -15,7 +14,7 @@ applicant_deserializer = ApplicantSchema()
 
 periods = ["day", "week", "month", "year"]
 
-@trends_api.route("/", methods=["GET", "POST", "PUT", "DELETE"])
+@trends_api.route("/", methods=["GET"])
 class TrendsApi(Resource):
     def get(self):
       query = base_query()
@@ -108,30 +107,44 @@ def split_into_year(unit, today, data_since_old_date, series):
 
   return data
 
-def split_into_month(unit, today, data_since_old_date, series):
+def split_into_month(unit, today, data_since_old_date, series, upper_bound_inclusive=True):
   data = []
   upper_bound = today
   lower_bound = today - relativedelta(months = 1)
   while unit > 0:
     for s in series:
-      data.append({"period": (lower_bound + relativedelta(days = 1)).strftime("%m/%d/%Y"), 
+      if (upper_bound_inclusive):
+        count = len([x for x in data_since_old_date if x['submitted'] > lower_bound and x['submitted'] <= upper_bound and x['series'] == s])
+        period = (lower_bound + relativedelta(days = 1)).strftime("%m/%d/%Y")
+      else:
+        count = len([x for x in data_since_old_date if x['submitted'] >= lower_bound and x['submitted'] < upper_bound and x['series'] == s])
+        period = lower_bound.strftime("%m/%d/%Y")
+      data.append({"period": period, 
       "series": s,
-      "count": len([x for x in data_since_old_date if x['submitted'] > lower_bound and x['submitted'] <= upper_bound and x['series'] == s])})
+      "count": count})
+    print("Upper bound (inclusive)", upper_bound)
+    print("Lower bound (exclusive)", lower_bound)
     upper_bound = lower_bound
     lower_bound = upper_bound - relativedelta(months = 1)
     unit -= 1
 
   return data
 
-def split_into_week(unit, today, data_since_old_date, series):
+def split_into_week(unit, today, data_since_old_date, series, upper_bound_inclusive=True):
   data = []
   upper_bound = today
   lower_bound = today - relativedelta(weeks = 1)
   while unit > 0:
     for s in series:
-      data.append({"period": (lower_bound + relativedelta(days = 1)).strftime("%m/%d/%Y"), 
+      if upper_bound_inclusive:
+        count = len([x for x in data_since_old_date if x['submitted'] > lower_bound and x['submitted'] <= upper_bound and x['series'] == s])
+        period = (lower_bound + relativedelta(days = 1)).strftime("%m/%d/%Y")
+      else:
+        count = len([x for x in data_since_old_date if x['submitted'] >= lower_bound and x['submitted'] < upper_bound and x['series'] == s])
+        period = lower_bound.strftime("%m/%d/%Y")
+      data.append({"period": period, 
       "series": s,
-      "count": len([x for x in data_since_old_date if x['submitted'] > lower_bound and x['submitted'] <= upper_bound and x['series'] == s])})
+      "count": count})
     upper_bound = lower_bound
     lower_bound = upper_bound - relativedelta(weeks = 1)
     unit -= 1
@@ -149,3 +162,93 @@ def split_into_day(unit, today, data_since_old_date, series):
     unit -= 1
 
   return data
+
+# for each "cycle", get all the data between start and end month for each period
+@trends_api.route("/cycle", methods=["GET"])
+class TrendsCycleApi(Resource):
+    def get(self):
+      query = base_query()
+
+      unit = request.args.get("unit", type=int)
+      period = request.args.get("period", default="year", type=str).lower()
+      cycles = request.args.get("cycle", default=None, type=str) # e.g. cycle=21,22,23
+      cumulative = request.args.get("cumulative", default=True, type=bool)
+      start =request.args.get("start", default=10, type=int)
+      end = request.args.get("end", default=10, type=int)
+      series = request.args.get("series", default=None, type=str)
+      program_code = request.args.get("code", default=None, type=str)
+      gender = request.args.get("gender", default=None, type=str)
+      fee_status = request.args.get("fee_status", default=None, type=str)
+      nationality = request.args.get("nationality", default=None, type=str)
+
+      if start < 10 and end > 10 or start > 10 and end == start:
+        return {"message": "Invalid time frame given"}, 400
+
+      if not series:
+        if program_code is not None:
+          query = query.filter(Applicant.program_code == program_code)
+        
+        if gender is not None:
+          query = query.filter(Applicant.gender == gender)
+
+        if fee_status is not None:
+          query = query.filter(Applicant.combined_fee_status == fee_status)
+
+        if nationality is not None:
+          query = query.filter(Applicant.nationality == nationality)
+      
+      cycles = cycles.split(",")
+      cycles = ["20" + cycle for cycle in cycles]
+      data = []
+
+      if series:
+        series_types = set([applicant_deserializer.dump(d)[series] for d in db.session.query(Applicant)])
+      else:
+        series_types = set(['ALL'])
+      print(series_types)
+      for cycle in cycles:
+        print(f"---------{cycle}--------")
+        applicants = [applicant_deserializer.dump(d) for d in query.filter(Applicant.admissions_cycle == int(cycle))]
+        print("got", len(applicants), "applicants from database")
+        if series:
+          applicants = list(map(lambda x: {'submitted': datetime.strptime(x['submitted'], "%Y-%m-%d").date(), 'series': x[series]}, applicants))
+        else:
+          applicants = list(map(lambda x: {'submitted': datetime.strptime(x['submitted'], "%Y-%m-%d").date(), 'series': 'ALL'}, applicants))
+        print(applicants)
+
+        # get "today's" date
+        if end > 10:
+          today = date(int(cycle), end, 1)
+        else:
+          today = date(int(cycle) + 1, end, 1)
+        
+        # get the "start" date
+        if start >= 10:
+          start_date = date(int(cycle), start, 1)
+        else:
+          start_date = date(int(cycle) + 1, start)
+        
+        if period == "month":
+          unit = (today.year - start_date.year) * 12 + today.month - start_date.month + 1
+        elif period == "week":
+          unit = abs(today - start_date).days // 7 + 1
+        elif period == "day":
+          unit = abs(today - start_date).days
+
+        print("Today's date:", today)
+        print("Oldest possible entry", start_date)
+        print("Going back", unit, period)
+
+        print(len(applicants))
+        if period == "month":
+          new_data = split_into_month(unit, today, applicants, series_types, upper_bound_inclusive=False)
+        elif period == "week":
+          new_data = split_into_week(unit, today, applicants, series_types, upper_bound_inclusive=False)
+        elif period == "day":
+          new_data = split_into_day(unit, today, applicants, series_types)
+        
+        for d in new_data:
+          d["series"] = cycle
+          data.append(d)
+      return data, 200
+      
