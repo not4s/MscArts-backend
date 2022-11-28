5b1c385a7f351b2ebcde129e1e374fae77e43fb5
Bin Sheng Ang <bsa220@ic.ac.uk>
2022-11-28 12:16:14 +0000

    Added API for comments

--- app/schemas/applicant.py
+++ app/schemas/applicant.py
@@ -1,21 +1,36 @@
 from app import ma
-from app.models.applicant import Program, Applicant, Target, ProgramMapping
+from app.models.applicant import (
+    Program,
+    Applicant,
+    Target,
+    ProgramMapping,
+    ApplicantComment,
+)
+
 
 class ProgramSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Program
         include_fk = True
 
+
 class ProgramMappingSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = ProgramMapping
+
 
 class ApplicantSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Applicant
         include_fk = True
 
+
 class TargetSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Target
         include_fk = True
+
+
+class ApplicantCommentSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = ApplicantComment
